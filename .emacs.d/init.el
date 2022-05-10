e638e3ba4b84c6c664056fde46cc353ac2bf4ce1
Francesco Carmelo Capria <francesco@capria.eu>
2022-05-10 16:48:26 +0200

    emacs add

---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" default))
+ '(package-selected-packages '(dracula-theme lua-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(menu-bar-mode -1) 
+
+(load-theme 'dracula)
